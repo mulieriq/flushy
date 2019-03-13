ba557bcc540fe154f5458c4ac30e819bc07bf96b
Eric Muli <erickiokom@gmail.com>
2019-03-13 16:00:12 +0300

    Dependancies

--- lib/userScreen/deliveryAddress.dart
+++ lib/userScreen/deliveryAddress.dart
@@ -1,7 +1,7 @@
 /*
 Author : Eric Muli
 Github : github.com/EricRootLee
-Page Title : Delivery Adress Section
+Page Title : Delivery Address Section
 Platform : Mobile
 Target : Android And IoS
 Technology : Flutter
